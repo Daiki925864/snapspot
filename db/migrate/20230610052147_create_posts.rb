d779c2b12862c9ba7e214007a3587ce0f16b036a
Daiki925864 <daikiokada9097@gmail.com>
2023-06-10 23:46:01 +0900

    DBの修正

--- db/migrate/20230610052147_create_posts.rb
+++ db/migrate/20230610052147_create_posts.rb
@@ -4,7 +4,6 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string :title,             null: false
       t.text   :content,           null: false
       t.date   :shooting_date,     null: false
-      t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,29 +16,8 @@ ActiveRecord::Schema.define(version: 2023_06_10_052147) do
     t.string "title", null: false
     t.text "content", null: false
     t.date "shooting_date", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "nickname", null: false
-    t.date "birthday", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "posts", "users"
 end
